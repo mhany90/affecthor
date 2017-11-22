b9f14a02977ffc97b301e3772d310d628ffac814
M.Abdou@student.rug.nl <s3094723@peregrine.hpc.rug.nl>
2017-11-22 16:36:00 +0100

    Fixed spanish scraper

--- scraper/es_scrape.py
+++ scraper/es_scrape.py
@@ -1,18 +1,20 @@
 import tweepy
 import sys
 
-auth = tweepy.AppAuthHandler('PTkRZpwIXTIXs2rFpC8bU7dN1',
-						   'Rz80z98fAlVwU6a4ckCzZ7U7pXXWYVGwRyxw7VkH48b7Qc4gtE')
+auth = tweepy.AppAuthHandler('FMwQZ2L8MM5Gzi0NVwWhsQFLD',
+						     'qciRPPQ9mEVm4RHIFlxos5lHNvm3Nq1yINcoUBHoOKxpYw5osH')
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-query = 'a OR e OR i OR o OR u'
+query = 'a OR e OR i OR o OR u OR y OR el OR la OR los OR las OR que OR q OR no OR es OR un OR por OR una OR lo OR de'
 limit = 10000000
 
 count = 0
-with open('es_dump.txt', 'w') as f:
+with open('es_dump.txt', 'a') as f:
 	for tweet in tweepy.Cursor(api.search,q=query,lang='es').items(limit):
+		print(tweet.text)
 		f.write("#{} {}\n".format(tweet.id, tweet.text))
 		count += 1
+		f.flush()
 		if count % 10000 == 0:
 			sys.stdout.write("Scraped {} tweets\n".format(count))
 
