ba89ef7b993cbf61a74e8c46a7af4b782a1776f9
Joan Ginés i Ametllé <joangia@coli.uni-saarland.de>
2017-11-25 01:12:30 +0100

    Update filter_remove.sh

--- utils/filter_remove.sh
+++ utils/filter_remove.sh
@@ -4,6 +4,11 @@
 #SBATCH --time=00:10:00
 #SBATCH --mem=30GB
 
+# Arguments:
+#  $1 : input file
+#  $2 : output file
+#  $3 : field to remove
+
 
 java -Xmx30G -cp weka.jar \
      weka.Run weka.filters.unsupervised.attribute.Remove -R $3 -i $1 -o $2
