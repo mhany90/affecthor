3d726b23c05638d20aa1604986b57a6d3d7f7a84
M.Abdou@student.rug.nl <s3094723@peregrine.hpc.rug.nl>
2017-12-08 15:01:25 +0100

    stupid averaging seems to help

---
+++ models/ensemble/dnn_gbt.py
@@ -0,0 +1,132 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.contrib.learn.python import SKCompat
+import tflearn
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import RandomForestRegressor
+from scipy.stats import pearsonr
+from sklearn.model_selection import cross_val_score
+from sklearn.utils import shuffle
+from sklearn.metrics import make_scorer
+from numpy import mean
+import os
+import pandas as pd
+from sklearn.preprocessing import normalize
+from sklearn.model_selection import KFold
+
+
+
+
+def read_and_shuffle(f):
+    df = pd.read_csv(f, header=None, skiprows=0)
+    df = shuffle(df)
+    #x = df.iloc[0:]
+    #y = df[0]
+    #return x.iloc[:-200], y.iloc[:-200], x.iloc[-200:], y.iloc[-200:]
+    return df
+
+def read_and_shuffle_cv(f):
+    df = pd.read_csv(f, header=None, skiprows=0)
+    df = shuffle(df)
+    x = df.iloc[:,1:]
+    y = df[0]
+    return x, y
+
+
+#def kfold(x):
+#    kf_total = KFold(len(x), n_folds=10, shuffle=True, random_state=4)
+#    return kf_total
+
+def pearson(y_pred, y_true):
+    p = -1* pearsonr(y_pred, y_true)[0]
+    p = convert_to_tensor(
+    p,
+    dtype=None,
+    name=None,
+    preferred_dtype=None)
+    return p
+
+
+def l1_norm(prediction, target, inputs):
+    return tf.reduce_sum(tf.abs(prediction - target), name='l1')
+
+
+#model
+network = input_data(shape=[None, 4139])
+network = fully_connected(network, 3000 , activation='relu')
+network = dropout(network, 0.8)
+network = fully_connected(network, 1500, activation='relu')
+network = dropout(network, 0.8)
+network = fully_connected(network, 750, activation='relu')
+network = dropout(network, 0.8)
+network = fully_connected(network, 350, activation='relu')
+network = dropout(network, 0.8)
+network = fully_connected(network, 150, activation='relu')
+network = dropout(network, 0.8)
+network = fully_connected(network, 75, activation='relu')
+network = tflearn.fully_connected(network, 1, activation='sigmoid')
+network = tflearn.regression(network, optimizer='adam', learning_rate=0.001,
+                         loss='mean_square', metric='R2' )
+
+
+
+
+#DIR = '/data/s3094723/extra_features/'
+
+
+#files = [DIR+f for f in os.listdir(DIR) if "sadness" in f]
+
+
+files = ['/data/s3094723/extra_features/EI-reg-En-joy-train.txt.sent.lex']
+#test_file = '/home/s3094723/SEMEVAL/affecthor/data/EI-reg/En/dev/'	
+
+
+
+#model = tflearn.DNN(network, tensorboard_verbose=0)
+#model.fit(train_x, train_y, validation_set=(test_x, test_y), show_metric=True,
+ #         batch_size=8)
+
+
+for f in files:
+    print(f)
+    data = read_and_shuffle(f)
+    correlations = []
+    kf = KFold(n_splits=10)
+    for train, test in kf.split(data):
+        print("index test: " ,test)
+        print("index train:", train)
+        train_data = data.iloc[train,:]
+        test_data = data.iloc[test,:]
+#        print(train_data)
+        train_y = train_data.iloc[:,0]
+        test_y = test_data.iloc[:,0]
+        train_data = train_data.drop(train_data.columns[0], axis=1)
+ #       print(train_data)
+        test_data = test_data.drop(test_data.columns[0], axis=1)
+        print("shape: ", test_data.shape, train_data.shape)
+        train_y_gbt = train_y.values   
+        train_y = np.reshape(train_y.values,(-1,1))
+        model =  tflearn.DNN(network, tensorboard_verbose=0)
+        model.fit(train_data.values, train_y,  show_metric = True, batch_size=10)
+        #gbt
+        regr = GradientBoostingRegressor(max_depth=3,n_estimators=450, learning_rate = 0.05,subsample=0.9, max_leaf_nodes=37000)
+        regr.fit(train_data.values, train_y_gbt)
+        predictions_gbt = regr.predict(test_data.values)
+
+        predictions_dnn = model.predict(test_data.values)
+        predictions_dnn = [item for sublist in predictions_dnn for item in sublist]
+        #avg
+        predictions = np.mean([predictions_gbt, predictions_dnn], axis = 0)
+   
+        for p, t in zip(predictions, test_y.values):
+            print('Test predictions: {}, Truth: {}'.format(p, t))
+
+        #predictions = [item for sublist in predictions for item in sublist]
+
+        corr = pearsonr(predictions, test_y.values)
+        print(corr[0])
+        correlations.append(corr[0])
+
+print("Mean Corr.:", np.mean(correlations))
